c3adabf7d84f3601ca24b3340ae2e581f2488b4e
Fernando <fernando.bordin.correa@gmail.com>
2025-08-20 20:03:01 -0300

    Frontend da pagina de registro feito com css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,21 @@ import Home from './components/pages/Home';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
 
 function App() {
   return (
     <Router>
       <Navbar/>
-      <Routes>
+      <Container>
+        <Routes>
 
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>}/>
         <Route path="/" element={<Home/>}/>
 
       </Routes>
+      </Container>
       <Footer/>
     </Router>
   );
--- frontend/src/components/pages/auth/Register.js
+++ frontend/src/components/pages/auth/Register.js
@@ -1,8 +1,28 @@
+import Input from '../../form/Input'
+import styles from '../../form/Form.module.css'
+
+import {Link} from 'react-router-dom'
 function Register() {
+
+    function handleOnChange(e) {
+        
+    }
+
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <section className={styles.form_container}>
+            <h1>Registrar</h1>
+            <form>
+                <Input type="text" text="Nome" name="name" placeholder="Digite o seu nome" handleOnChange={handleOnChange}/>
+                <Input type="text" text="Telefone" name="phone" placeholder="Digite o seu telefone" handleOnChange={handleOnChange}/>
+                <Input type="E-mail" text="email" name="email" placeholder="Digite o seu email" handleOnChange={handleOnChange}/>
+                <Input type="password" text="senha" name="password" placeholder="Digite a sua senha" handleOnChange={handleOnChange}/>
+                <Input type="password" text="Confirmacao de senha" name="confirmpassword" placeholder="Confirme a sua senha" handleOnChange={handleOnChange}/>
+                <input type="submit" value="Cadastrar"/>
+            </form>
+            <p>
+                Ja tem conta? <Link to="/login">Clique aqui.</Link> 
+            </p>
+        </section>
     )
 }
 
